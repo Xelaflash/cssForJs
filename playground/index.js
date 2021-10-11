228ced0f5d0aaf7a72a183f7c29a7d5c000715e0
Alexandre Gros <xelaflash@gmail.com>
2021-10-11 10:22:06 -0400

    Init git for this repo

---
+++ playground/index.js
@@ -0,0 +1,11 @@
+console.log('Hello World');
+
+class Main {
+  color = 'black';
+}
+class Paragraph extends Main {
+  color = 'red';
+}
+class Span extends Paragraph {}
+const s = new Span();
+console.log(s.color);
